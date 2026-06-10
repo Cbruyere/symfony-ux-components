be505728e44a36a5616dd5a65e3e908c007aa5e6
Christophe Bruyere <christophe.developpement@gmail.com>
2026-06-10 06:07:05 +0200

    (feat) Add Badge component

---
+++ src/Twig/Components/Badge.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ChrisDev\UxComponents\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(
+    name: 'Badge',
+    template: '@ChrisDevUxComponents/components/Badge.html.twig'
+)]
+final class Badge
+{
+    public string $variant = 'neutral';
+    public string $label = '';
+
+    public function getVariantClasses(): string
+    {
+        return match ($this->variant) {
+            'info' => 'bg-blue-800 text-blue-300',
+            'success' => 'bg-green-800 text-green-300',
+            'warning' => 'bg-amber-800 text-amber-300',
+            'danger' => 'bg-red-800 text-red-300',
+            default => 'bg-slate-800 text-slate-300',
+        };
+    }
+}
